298e103869144c58b993bd70525ef5111b80147e
ma_harada <harada@respawn.tokyo>
2022-03-19 15:32:43 +0900

    initial

---
+++ lib/hello-cdk-stack.ts
@@ -0,0 +1,15 @@
+import { Stack, StackProps, aws_s3 as s3 } from 'aws-cdk-lib'
+import { Bucket } from 'aws-cdk-lib/aws-s3'
+import { Construct } from 'constructs'
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class HelloCdkStack extends Stack {
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id, props)
+		// AWS S3にバケット「hello-world-bucket-202203190000」を追加する
+		// バケットのバージョニングを有効に設定
+		new s3.Bucket(this, 'hello-world-bucket-202203190000', {
+			versioned: true,
+		})
+	}
+}
