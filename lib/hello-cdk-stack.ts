e94a4f2b3ce5e2d58f120e7c802354c289732f32
ma_harada <harada@respawn.tokyo>
2022-03-19 16:23:32 +0900

    setting s3 bucket name

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,5 +1,4 @@
 import { Stack, StackProps, aws_s3 as s3 } from 'aws-cdk-lib'
-import { Bucket } from 'aws-cdk-lib/aws-s3'
 import { Construct } from 'constructs'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -8,9 +7,10 @@ export class HelloCdkStack extends Stack {
 		super(scope, id, props)
 		// AWS S3にバケット「hello-world-bucket-202203190000」を追加する
 		// バケットのバージョニングを有効に設定
-		const bucket: string =
-			process.env.S3_BUCKET_NAME !== undefined ? process.env.S3_BUCKET_NAME : ''
-		new s3.Bucket(this, bucket, {
+		const bucketName: string = this.node.tryGetContext('s3').bucketName
+		process.env.S3_BUCKET_NAME !== undefined ? process.env.S3_BUCKET_NAME : ''
+		new s3.Bucket(this, 'S3Bucket', {
+			bucketName: bucketName,
 			versioned: true,
 		})
 	}
